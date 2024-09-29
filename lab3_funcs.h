62e0b3f3c5447507e6e076c3291e22dcd56edafd
xzhangfordham <42500181+xzhangfordham@users.noreply.github.com>
2024-09-29 16:15:36 -0400

    Update lab3_funcs.h

--- lab3_funcs.h
+++ lab3_funcs.h
@@ -4,25 +4,9 @@
 #include <functional>
 using namespace std;
 
-void SelectionSort(vector<int> & L);
-
-// recursive selection sort
-void SelectionSort(vector<int> & list, int left, int right) ;
-
-// iterative bubble sort
-void BubbleSort(vector<int> & L);
-
-// recursive bubble sort
-void BubbleSortRec(vector<int> & list, int left, int right);
-
-// binary search a list
-int BinarySearch(const vector<int> & list, int target, int left, int right);
-
-//Functions from lab1
-
+//Three helper functions 
 vector<int> GenerateVectorFromInput ();
 vector<int> GenerateRandomVector(int size);
-
 
 
 /* Print a vector of int to cerr, e.g.,
@@ -30,12 +14,10 @@ Current vector: 13 9 23 12 29
 */
 void PrintVector (const vector<int> & L);
 
-
-//new functions to write for lab3
-
+//Functions to be implemented in lab2 
 void Merge (const vector<int> & list1, const vector<int> & list2, vector<int> & result);
 void MergeSort (vector<int> & L);
 void QuickSort (vector<int> & L, int first, int last);
 void KWayMerge_BF (vector<vector<int>> listOfLists, vector<int> & result);
 void KWayMerge_DAC (vector<vector<int>> listOfLists, vector<int> & result);
-void TimSort (vector<int> & list, int run_length);
+void HybridSort (vector<int> & list, int run_length);
